488d9df9dd08aa0ed3dd3e72c32b48a2216fc1df
byhieg <byhieg@gmail.com>
2023-03-26 08:55:14 +0800

    初始化spring集成Junit5代码

---
+++ spring-boot-integration-junit5/src/test/java/com/byhieg/SpringBootIntegrationJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byhieg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntegrationJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
